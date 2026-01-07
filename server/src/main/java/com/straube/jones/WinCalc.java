f279b9c2813b8fc43477c24a8482b2e2c13d8a67
mark <mark.straube@t-online.de>
2026-01-07 14:11:42 +0100

    added example

--- server/src/main/java/com/straube/jones/WinCalc.java
+++ server/src/main/java/com/straube/jones/WinCalc.java
@@ -14,6 +14,47 @@ public class WinCalc
             capital += win - win * 0.25; // minus tax
             System.out.println("Year " + (i + 1) + ": " + String.format("%.2f", capital));
         }
-        System.out.println("Prozentsatz: " + capital / Startcapital);
+        System.out.println("Capital - after tax: " + String.format("%.2f", capital));
+        System.out.println("Prozentsatz: "
+                        + String.format("%.2f", (capital - Startcapital) / Startcapital * 100)
+                        + "%");
+
+        capital = Startcapital;
+        for (int i = 0; i < 24; i++ )
+        {
+            double win = capital * 0.1;
+            capital += win; // without tax
+            System.out.println("Year " + (i + 1) + ": " + String.format("%.2f", capital));
+        }
+        System.out.println("Capital - before tax: " + String.format("%.2f", capital));
+        capital = capital - Startcapital - capital * 0.25;
+        System.out.println("Capital - after tax: " + String.format("%.2f", capital));
+        System.out.println("Prozentsatz: "
+                        + String.format("%.2f", (capital - Startcapital) / Startcapital * 100)
+                        + "%");
+
+        Double capital1 = Startcapital;
+        Double capital2 = Startcapital;
+        for (int i = 0; i < 10; i++ )
+        {
+            Double win = Startcapital * 0.1; // 10% pro Iteration
+            capital1 += win - win * 0.25;
+            capital2 += win;
+            System.out.println("Iteration " + (i + 1)
+                            + ": "
+                            + String.format("%.2f", capital1)
+                            + " / "
+                            + String.format("%.2f", capital2));
+        }
+        System.out.println("Capital1 - after tax: " + String.format("%.2f", capital1));
+        System.out.println("Capital2 - after tax: " + String.format("%.2f", capital2 - (capital2 - Startcapital) * 0.25) );
+
+    }
+
+
+    private static double fnc_Stock1(int iteration, double startCapital)
+    {
+        double capital = startCapital + startCapital * 0.1 * iteration;
+        return capital;
     }
 }
